ac886310baf8f3740e96dbabb3b4012c7eb1c883
leonardo salas <leosalass@gmail.com>
2020-07-29 23:30:04 -0500

    added mongo db connection

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const { Mongoose } = require("mongoose")
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/starter-kit', {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
+.then(db => console.log('Connected to mongodb'))
+.catch(err => console.error(err));
--- src/server.js
+++ src/server.js
@@ -6,18 +6,19 @@ const session = require('express-session');
 
 //Initializations
 const app = express();
+require('./config/database');
 
 //Settings
-app.set('port', process.env.PORT || 3000)
-app.set('App', path.join(__dirname, 'app'))
+app.set('port', process.env.PORT || 3000);
+app.set('App', path.join(__dirname, 'app'));
 
 //Middlewares
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true,
-}))
+}));
 //Global Variables
 
 //Routes
@@ -25,8 +26,8 @@ app.use(require('./routes/api.js'));
 app.use(require('./routes/auth.js'));
 
 //Static Files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 //Server is listening
 app.listen(app.get('port'), () => {
-  console.log(`Server listening on port:${app.get('port')} Mode:${process.env.NODE_ENV}`);
-})
+  console.log(`Server listening on port:${app.get('port')}, server mode:${process.env.NODE_ENV}`);
+});
