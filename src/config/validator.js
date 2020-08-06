cc37681b6fe8ae098a5c51e4a25f5d722923601f
leonardo salas <leosalass@gmail.com>
2020-08-06 01:45:03 -0500

    added another custom validation rule

--- src/config/validator.js
+++ src/config/validator.js
@@ -13,7 +13,25 @@ Validator.registerAsync('available', function(value, attribute, req, passes) {
   
   Model.findOne({[field]: value }, function (err, docs) {
     if(docs != null){
-      passes(false, `${field} has already been taken.`); 
+      passes(false, `${field}-available-validation-error`); 
+    }
+    passes();
+  });
+   
+});
+
+Validator.registerAsync('exists', function(value, attribute, req, passes) {
+  console.log('------------------------')
+
+  attribute = attribute.split('.')
+  const modelName = attribute[0]
+  const field = attribute[1]
+  
+  var Model = mongoose.model(modelName);
+  
+  Model.findOne({[field]: value }, function (err, docs) {
+    if(docs == null){
+      passes(false, `${field}-exists-validation-error`); 
     }
     passes();
   });
